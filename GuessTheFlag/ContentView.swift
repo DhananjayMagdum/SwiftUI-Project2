7f731f31cd09648d3ce289dddc9427c6774758d2
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2021-03-20 06:34:22 +0530

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Dhananjay Magdum on 20/03/21.
+//  Copyright © 2021 Atlas Copco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
