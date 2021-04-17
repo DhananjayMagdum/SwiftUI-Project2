63edc140088792281a811e150175ac227be31a80
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2021-04-17 15:43:34 +0530

    complete project commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var wrongAnswer = 0
+    @State private var totalClicks = 0
+    @State private var correctAnswers = 0
+    @State private var arrayOfFlagsDisplayed = [String]()
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack(spacing: 10) {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    Text("SCORE: \(correctAnswers) \\ \(totalClicks)")
+                }
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+            }
+        }.alert(isPresented: $showingScore) {
+            let message  = scoreTitle == "Correct" ? "Your score is \(correctAnswers) \\ \(totalClicks)" : "That's the flag of \(countries[wrongAnswer])"
+            return Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
+                    self.askQuestion()
+                })
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        totalClicks += 1
+        if number==correctAnswer {
+            scoreTitle = "Correct"
+            correctAnswers += 1
+        } else {
+            scoreTitle = "Wrong"
+            wrongAnswer = number
+        }
+        showingScore = true
     }
 }
 
